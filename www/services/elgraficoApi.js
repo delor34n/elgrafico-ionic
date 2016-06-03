35cada2d7bab17d016ede875ab417a6437dd95fe
sebastian.lastra <sebastian.lastra@cursor.cl>
2016-06-03 12:26:43 -0300

    mostrando mensaje 'cargando noticias'.

--- www/services/elgraficoApi.js
+++ www/services/elgraficoApi.js
@@ -4,21 +4,25 @@
 (function(){
   'use strict';
 
-  angular.module('elgrafico-ionic').factory('elgraficoApi', ['$http', '$q', elgraficoApi]);
+  angular.module('elgrafico-ionic').factory('elgraficoApi', ['$http', '$q', '$ionicLoading', elgraficoApi]);
 
-  function elgraficoApi($http, $q){
+  function elgraficoApi($http, $q, $ionicLoading){
 
 
     function getNews(){
       var deferred = $q.defer();
+
+      $ionicLoading.show({template: 'Cargando noticias...'});
       $http.get("https://elgrafico.herokuapp.com/futbolchileno/getAllLastNews")
         .success(function(data, status){
           var bigArray = [].concat.apply([], data);
           console.log("Recibiendo noticias ", bigArray, status);
+          $ionicLoading.hide();
           deferred.resolve(bigArray);
         })
         .error(function(){
           console.error("Error en la llamada HTTP.");
+          $ionicLoading.hide();
           deferred.reject();
         });
       return deferred.promise;
